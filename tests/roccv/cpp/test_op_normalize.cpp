3c63d12ef1db9a8f61f521757a2546262c6cb94c
Jeff Jiang <jeffq.jiang@amd.com>
2025-08-12 14:45:39 -0400

    * rocCV/Normalize operator: Minor format change based on reivew.

--- tests/roccv/cpp/test_op_normalize.cpp
+++ tests/roccv/cpp/test_op_normalize.cpp
@@ -64,7 +64,7 @@ void GenerateGoldenNormalize(std::vector<BT>& input, std::vector<BT>& output, Si
             for (int c = 0; c < imgFormat.channels(); c++) {
                 int base_c = (baseFormat.channels() == 1 ? 0 : c);
                 int scale_c = (scaleFormat.channels() == 1 ? 0 : c);
-                float scaleFactor = scale.at(scale_y * scaleSize.w * scaleFormat.channels() + scale_x * scaleFormat.channels() + scale_c);;
+                float scaleFactor = scale.at(scale_y * scaleSize.w * scaleFormat.channels() + scale_x * scaleFormat.channels() + scale_c);
                 if (scaleIsStdDev) {
                     scaleFactor = 1.0 / std::sqrt(scaleFactor * scaleFactor + epsilon);
                 }
