b95c6af4a2e52718173e26ab117efebc0f1dc40b
Essam Aly <essam.aly@amd.com>
2025-08-13 13:10:57 -0700

    optimize & cleanup color cvt test code

--- tests/roccv/cpp/test_op_cvt_color.cpp
+++ tests/roccv/cpp/test_op_cvt_color.cpp
@@ -32,105 +32,85 @@ using namespace roccv::tests;
 namespace fs = std::filesystem;
 
 namespace {
-eTestStatusType testCorrectness(const std::string &inputFile, uint8_t *expectedData,
-                                eColorConversionCode conversionCode, eDeviceType device) {
+eTestStatusType testCorrectness(const std::string &inputFile, uint8_t *expectedData, eColorConversionCode conversionCode, eDeviceType device) {
+
     cv::Mat image_data = cv::imread(inputFile);
-
-    TensorShape shape(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC),
-                      {1, image_data.rows, image_data.cols, image_data.channels()});
-
-    TensorShape shape_grayscale(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC),
-                                {1, image_data.rows, image_data.cols, 1});
-
+    TensorShape shape_clr(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, image_data.rows, image_data.cols, image_data.channels()});
+    TensorShape shape_gry(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, image_data.rows, image_data.cols, 1});
     DataType dtype(eDataType::DATA_TYPE_U8);
+    Tensor input__clr(shape_clr, dtype, device);
+    Tensor output_clr(shape_clr, input__clr.dtype(), input__clr.device());
+    Tensor output_gry(shape_gry, input__clr.dtype(), input__clr.device());
 
-    Tensor input(shape, dtype, device);
-    Tensor output(shape, input.dtype(), input.device());
-    Tensor output_grayscale(shape_grayscale, input.dtype(), input.device());
-
-    if (device == eDeviceType::GPU) {
-        hipStream_t stream;
+    size_t image_size = input__clr.shape().size() * input__clr.dtype().size();
+    auto inputData = input__clr.exportData<TensorDataStrided>();
+    CvtColor op;
+    int tst_limit = 0;
+    hipStream_t stream = static_cast<hipStream_t>(nullptr);
+    if(device == eDeviceType::GPU) {
         HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
+        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(inputData.basePtr(), image_data.data, image_size, hipMemcpyHostToDevice, stream));
+    } else { // CPU
+        memcpy(inputData.basePtr(), image_data.data, image_size);
+    }
+    size_t output_image_size = 0;
+    std::vector<uint8_t> resultData;
+    std::optional<TensorDataStrided> outputTensorData;
 
-        size_t image_size = input.shape().size() * input.dtype().size();
-        auto inputData = input.exportData<TensorDataStrided>();
-        HIP_VALIDATE_NO_ERRORS(
-            hipMemcpyAsync(inputData.basePtr(), image_data.data, image_size, hipMemcpyHostToDevice, stream));
-
-        CvtColor op;
-        if (conversionCode == eColorConversionCode::COLOR_RGB2GRAY ||
-            conversionCode == eColorConversionCode::COLOR_BGR2GRAY) {
-            op(stream, input, output_grayscale, conversionCode, device);
-            size_t output_image_size = output_grayscale.shape().size() * output.dtype().size();
-            auto outputTensorData = output_grayscale.exportData<TensorDataStrided>();
-            std::vector<uint8_t> resultData(output_image_size);
-            HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(resultData.data(), outputTensorData.basePtr(), output_image_size,
-                                                  hipMemcpyDeviceToHost, stream));
-            HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
-            for (int i = 0; i < output_image_size; i++) {
-                float err = std::abs(resultData[i] - expectedData[i]);
-                if (err > 1) {
-                    std::cout << "CvtColor (DEVICE) failed at index: " << i << " with an error of: " << err
-                              << std::endl;
-                    return eTestStatusType::UNEXPECTED_VALUE;
-                }
-            }
-        } else {
-            op(stream, input, output, conversionCode, device);
-            size_t output_image_size = output.shape().size() * output.dtype().size();
-            auto outputTensorData = output.exportData<TensorDataStrided>();
-            std::vector<uint8_t> resultData(output_image_size);
-            HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(resultData.data(), outputTensorData.basePtr(), output_image_size,
-                                                  hipMemcpyDeviceToHost, stream));
-            HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
-            for (int i = 0; i < output_image_size; i++) {
-                float err = std::abs(resultData[i] - expectedData[i]);
-                if (err > 1) {
-                    std::cout << "CvtColor (DEVICE) failed at index: " << i << " with an error of: " << err
-                              << std::endl;
-                    return eTestStatusType::UNEXPECTED_VALUE;
-                }
+    switch(conversionCode) {
+        case eColorConversionCode::COLOR_RGB2GRAY:
+        case eColorConversionCode::COLOR_BGR2GRAY:
+        {
+            op(stream, input__clr, output_gry, conversionCode, device);
+            output_image_size = output_gry.shape().size() * output_clr.dtype().size();
+            outputTensorData.emplace(output_gry.exportData<TensorDataStrided>());
+            resultData.resize(output_image_size);
+            if(device == eDeviceType::CPU) {
+                auto* base = outputTensorData->basePtr();
+                memcpy(resultData.data(), base, output_gry.shape().size() * output_gry.dtype().size());
+                tst_limit = output_gry.shape().size();
             }
         }
-        HIP_VALIDATE_NO_ERRORS(hipStreamDestroy(stream));
-    } else if (device == eDeviceType::CPU) {
-        size_t image_size = input.shape().size() * input.dtype().size();
-        auto inputData = input.exportData<TensorDataStrided>();
-        memcpy(inputData.basePtr(), image_data.data, image_size);
+        break;
 
-        CvtColor op;
-        if (conversionCode == eColorConversionCode::COLOR_RGB2GRAY ||
-            conversionCode == eColorConversionCode::COLOR_BGR2GRAY) {
-            op(nullptr, input, output_grayscale, conversionCode, device);
-            size_t output_image_size = output_grayscale.shape().size() * output.dtype().size();
-            auto outputTensorData = output_grayscale.exportData<TensorDataStrided>();
-            std::vector<uint8_t> resultData(output_image_size);
-            memcpy(resultData.data(), outputTensorData.basePtr(),
-                   output_grayscale.shape().size() * output_grayscale.dtype().size());
-            for (int i = 0; i < output_grayscale.shape().size(); i++) {
-                float err = std::abs(resultData[i] - expectedData[i]);
-                if (err > 1) {
-                    std::cout << "CvtColor (HOST) failed at index: " << i << " with an error of: " << err << std::endl;
-                    return eTestStatusType::UNEXPECTED_VALUE;
-                }
+        default: // COLOR = any other than GRAY
+        {
+            op(stream, input__clr, output_clr, conversionCode, device);
+            output_image_size = output_clr.shape().size() * output_clr.dtype().size();
+            outputTensorData.emplace(output_clr.exportData<TensorDataStrided>());
+            resultData.resize(output_image_size);
+            if(device == eDeviceType::CPU) {
+                auto* base = outputTensorData->basePtr();
+                memcpy(resultData.data(), base, output_clr.shape().size() * output_clr.dtype().size());
+                tst_limit = output_clr.shape().size();
             }
-        } else {
-            op(nullptr, input, output, conversionCode, device);
-            size_t output_image_size = output.shape().size() * output.dtype().size();
-            auto outputTensorData = output.exportData<TensorDataStrided>();
-            std::vector<uint8_t> resultData(output_image_size);
-            memcpy(resultData.data(), outputTensorData.basePtr(), output.shape().size() * output.dtype().size());
-            for (int i = 0; i < output.shape().size(); i++) {
-                float err = std::abs(resultData[i] - expectedData[i]);
-                if (err > 1) {
-                    std::cout << "CvtColor (HOST) failed at index: " << i << " with an error of: " << err << std::endl;
-                    return eTestStatusType::UNEXPECTED_VALUE;
-                }
-            }
+        }
+        break;
+    }
+
+    // test on GPU
+    if(device == eDeviceType::GPU) {
+        auto* base = outputTensorData->basePtr();
+        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(resultData.data(), base, output_image_size, hipMemcpyDeviceToHost, stream));
+        HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
+        tst_limit = output_image_size;
+    }
+    // final test
+    for (int i = 0; i < tst_limit; i++) {
+        float err = std::abs(resultData[i] - expectedData[i]);
+        if (err > 1) {
+            std::cout << "CvtColor " << ((device == eDeviceType::GPU) ? "(GPU)" : "(HOST)") << " failed at index: " << i << " with an error of: " << err << std::endl;
+            return eTestStatusType::UNEXPECTED_VALUE;
         }
     }
+    // clean up
+    if(device == eDeviceType::GPU) {
+        HIP_VALIDATE_NO_ERRORS(hipStreamDestroy(stream));
+    }
+
     return eTestStatusType::TEST_SUCCESS;
 }
+
 }  // namespace
 
 eTestStatusType test_op_cvt_color(int argc, char **argv) {
@@ -139,41 +119,29 @@ eTestStatusType test_op_cvt_color(int argc, char **argv) {
         return eTestStatusType::TEST_FAILURE;
     }
     fs::path testDataPath = fs::path(argv[1]) / "tests" / "ops";
+    fs::path tst_i = testDataPath / "test_input.bmp";
+    fs::path tst_y = testDataPath / "expected_color_cvt_yuv.bmp";
+    eTestStatusType success = eTestStatusType::TEST_SUCCESS;
+    eDeviceType gpu = eDeviceType::GPU;
+    eDeviceType cpu = eDeviceType::CPU;
 
     try {
         cv::Mat expectedDataYUV = cv::imread(testDataPath / "expected_color_cvt_yuv.bmp");
         cv::Mat expectedDataBGR = cv::imread(testDataPath / "expected_color_cvt_bgr.bmp");
         cv::Mat expectedDataRGB = cv::imread(testDataPath / "expected_color_cvt_rgb.bmp");
-        cv::Mat expectedDataGrayscale =
-            cv::imread(testDataPath / "expected_color_cvt_grayscale.bmp", cv::IMREAD_UNCHANGED);
+        cv::Mat expectedDataGrayscale = cv::imread(testDataPath / "expected_color_cvt_grayscale.bmp", cv::IMREAD_UNCHANGED);
 
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataYUV.data,
-                                           eColorConversionCode::COLOR_BGR2YUV, eDeviceType::GPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "expected_color_cvt_yuv.bmp", expectedDataBGR.data,
-                                           eColorConversionCode::COLOR_YUV2BGR, eDeviceType::GPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataRGB.data,
-                                           eColorConversionCode::COLOR_BGR2RGB, eDeviceType::GPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataGrayscale.data,
-                                           eColorConversionCode::COLOR_BGR2GRAY, eDeviceType::GPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataYUV.data,
-                                           eColorConversionCode::COLOR_BGR2YUV, eDeviceType::CPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "expected_color_cvt_yuv.bmp", expectedDataBGR.data,
-                                           eColorConversionCode::COLOR_YUV2BGR, eDeviceType::CPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataRGB.data,
-                                           eColorConversionCode::COLOR_BGR2RGB, eDeviceType::CPU),
-                           eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(testCorrectness(testDataPath / "test_input.bmp", expectedDataGrayscale.data,
-                                           eColorConversionCode::COLOR_BGR2GRAY, eDeviceType::CPU),
-                           eTestStatusType::TEST_SUCCESS);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataYUV.data, eColorConversionCode::COLOR_BGR2YUV, gpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_y, expectedDataBGR.data, eColorConversionCode::COLOR_YUV2BGR, gpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataRGB.data, eColorConversionCode::COLOR_BGR2RGB, gpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataGrayscale.data, eColorConversionCode::COLOR_BGR2GRAY, gpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataYUV.data, eColorConversionCode::COLOR_BGR2YUV, cpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_y, expectedDataBGR.data, eColorConversionCode::COLOR_YUV2BGR, cpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataRGB.data, eColorConversionCode::COLOR_BGR2RGB, cpu), success);
+        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataGrayscale.data, eColorConversionCode::COLOR_BGR2GRAY, cpu), success);
     } catch (Exception e) {
         std::cout << "Exception: " << e.what() << std::endl;
         return eTestStatusType::TEST_FAILURE;
     }
-    return eTestStatusType::TEST_SUCCESS;
+    return success;
 }
