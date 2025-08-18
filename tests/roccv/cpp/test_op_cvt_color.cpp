e03adddd544000c76fb813c3d122d875e966044e
Essam Aly <essam.aly@amd.com>
2025-08-18 11:31:53 -0700

    Adding C Modal Golden func for cvt color test source

--- tests/roccv/cpp/test_op_cvt_color.cpp
+++ tests/roccv/cpp/test_op_cvt_color.cpp
@@ -20,128 +20,183 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <filesystem>
+#include <hip/hip_runtime.h>
 #include <iostream>
-#include <op_cvt_color.hpp>
-#include <opencv2/opencv.hpp>
-#include <test_helpers.hpp>
-#include <vector>
+#include "op_cvt_color.hpp"
+#include "test_helpers.hpp"
+#include "common/array_wrapper.hpp"
+#include "common/math_vector.hpp"
+#include "common/strided_data_wrap.hpp"
+#include "core/wrappers/image_wrapper.hpp"
+#include "common/validation_helpers.hpp"
+#include "core/tensor.hpp"
+#include "common/conversion_helpers.hpp"
+#include "kernels/device/cvt_color_device.hpp"
+#include "kernels/host/cvt_color_host.hpp"
 
 using namespace roccv;
 using namespace roccv::tests;
+
 namespace fs = std::filesystem;
 
+// Keep all non-entrypoint functions in an anonymous namespace to prevent redefinition errors across translation units.
 namespace {
-eTestStatusType testCorrectness(const std::string &inputFile, uint8_t *expectedData, eColorConversionCode conversionCode, eDeviceType device) {
 
-    cv::Mat image_data = cv::imread(inputFile);
-    TensorShape shape_clr(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, image_data.rows, image_data.cols, image_data.channels()});
-    TensorShape shape_gry(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, image_data.rows, image_data.cols, 1});
-    DataType dtype(eDataType::DATA_TYPE_U8);
-    Tensor input__clr(shape_clr, dtype, device);
-    Tensor output_clr(shape_clr, input__clr.dtype(), input__clr.device());
-    Tensor output_gry(shape_gry, input__clr.dtype(), input__clr.device());
+/**
+ * @brief Verified golden C++ model for the Regular Color Convert operation.
+ *
+ * @tparam T Vectorized datatype of the image's pixels.
+ * @tparam BT Base type of the image's data.
+ * @param[in] input An input vector containing image data.
+ * @param[in] output An output vector containing result image data, to be filled.
+ * @param[in] conversionCode the conversion code, from enum eColorConversionCode.
+ * @return Vector containing the results of the operation.
+ */
+template <typename T, typename BT = detail::BaseType<T>>
+std::vector<BT> GoldenGammaColorCvt(const Tensor &input, const Tensor &output, int batch, int width, int height, const eColorConversionCode conversionCode) {
+    ImageWrapper<T> inputWrapper(input);
+    ImageWrapper<T> outputWrapper(output);
+    using PairNdxDlta = std::tuple<int, float>;
+    static const std::unordered_map<eColorConversionCode, PairNdxDlta> ndx_dlt = {
+        { COLOR_RGB2YUV , {0, 128.0f}},
+        { COLOR_BGR2YUV , {2, 128.0f}},
+        { COLOR_YUV2RGB , {0, 128.0f}},
+        { COLOR_YUV2BGR , {2, 128.0f}},
+        { COLOR_RGB2BGR , {0,   2.0f}},
+        { COLOR_BGR2RGB , {2,   0.0f}},
+        { COLOR_RGB2GRAY, {0,   0.0f}},
+        { COLOR_BGR2GRAY, {2,   0.0f}}
+    };
+    auto [orderIdx, delta] = ndx_dlt.at(conversionCode);
 
-    size_t image_size = input__clr.shape().size() * input__clr.dtype().size();
-    auto inputData = input__clr.exportData<TensorDataStrided>();
-    CvtColor op;
-    int tst_limit = 0;
-    hipStream_t stream = static_cast<hipStream_t>(nullptr);
-    if(device == eDeviceType::GPU) {
-        HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
-        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(inputData.basePtr(), image_data.data, image_size, hipMemcpyHostToDevice, stream));
-    } else { // CPU
-        memcpy(inputData.basePtr(), image_data.data, image_size);
+    switch (conversionCode) {
+    case COLOR_RGB2YUV:
+    case COLOR_BGR2YUV:
+        {
+            Kernels::Host::rgb_or_bgr_to_yuv<T>(inputWrapper, outputWrapper, orderIdx, delta);
+        }
+        break;
+    case COLOR_YUV2RGB:
+    case COLOR_YUV2BGR:
+        {
+            Kernels::Host::yuv_to_rgb_or_bgr<T>(inputWrapper, outputWrapper, orderIdx, delta);
+        }
+        break;
+    case COLOR_RGB2BGR:
+    case COLOR_BGR2RGB:
+        {
+            Kernels::Host::rgb_or_bgr_to_bgr_or_rgb<T>(inputWrapper, outputWrapper, orderIdx, delta);
+        }
+        break;
+    case COLOR_RGB2GRAY:
+    case COLOR_BGR2GRAY:
+        {
+            Kernels::Host::rgb_or_bgr_to_grayscale<T>(inputWrapper, outputWrapper, orderIdx);
+        }
+        break;
+    default:
+        break;
     }
-    size_t output_image_size = 0;
-    std::vector<uint8_t> resultData;
-    std::optional<TensorDataStrided> outputTensorData;
+    std::vector<BT> result;
+    result.resize(output.shape().size());
+    CopyTensorIntoVector(result, output);
+    return result;
+}
 
+/**
+ * @brief Tests correctness of the CVT Color operator, comparing it against a generated golden result.
+ *
+ * @tparam T Underlying datatype of the image's pixels.
+ * @tparam BT Base type of the image data.
+ * @param[in] width The width of each image in the batch.
+ * @param[in] height The height of each image in the batch.
+ * @param[in] format The image format.
+ * @param[in] conversionCode the conversion code, from enum eColorConversionCode.
+ * @param[in] device The device this correctness test should be run on.
+ */
+template <typename T, typename BT = detail::BaseType<T>>
+void TestCorrectness(int batch, int width, int height, ImageFormat format, eColorConversionCode conversionCode, eDeviceType device) {
+    TensorShape shape_clr(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, height, width, 3});
+    TensorShape shape_gry(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, height, width, 1});
+    DataType dtype(eDataType::DATA_TYPE_U8);
+    Tensor  input_clr(shape_clr, dtype, device);
+    Tensor output_clr(shape_clr, input_clr.dtype(), input_clr.device());
+    Tensor output_gry(shape_gry, input_clr.dtype(), input_clr.device());
+    size_t image_size = input_clr.shape().size() * input_clr.dtype().size();
+
+    // Create a vector and fill it with random data.
+    std::vector<BT> inputData(image_size);
+    FillVector(inputData);
+
+    // Copy generated input data into input tensor
+    CopyVectorIntoTensor(input_clr, inputData);
+
+    // Calculate golden output reference
+    std::vector<BT> ref;
+    CvtColor op;
+
+    hipStream_t stream = static_cast<hipStream_t>(nullptr);
+    if(device == eDeviceType::GPU)
+        HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
+
+    size_t output_image_size = 0;
     switch(conversionCode) {
         case eColorConversionCode::COLOR_RGB2GRAY:
         case eColorConversionCode::COLOR_BGR2GRAY:
         {
-            op(stream, input__clr, output_gry, conversionCode, device);
+            ref = GoldenGammaColorCvt<T>(input_clr, output_gry, batch, width, height, conversionCode);
+            op(stream, input_clr, output_gry, conversionCode, device);
             output_image_size = output_gry.shape().size() * output_clr.dtype().size();
-            outputTensorData.emplace(output_gry.exportData<TensorDataStrided>());
-            resultData.resize(output_image_size);
-            if(device == eDeviceType::CPU) {
-                auto* base = outputTensorData->basePtr();
-                memcpy(resultData.data(), base, output_gry.shape().size() * output_gry.dtype().size());
-                tst_limit = output_gry.shape().size();
-            }
         }
         break;
-
         default: // COLOR = any other than GRAY
         {
-            op(stream, input__clr, output_clr, conversionCode, device);
+            ref = GoldenGammaColorCvt<T>(input_clr, output_clr, batch, width, height, conversionCode);
+            op(stream, input_clr, output_clr, conversionCode, device);
             output_image_size = output_clr.shape().size() * output_clr.dtype().size();
-            outputTensorData.emplace(output_clr.exportData<TensorDataStrided>());
-            resultData.resize(output_image_size);
-            if(device == eDeviceType::CPU) {
-                auto* base = outputTensorData->basePtr();
-                memcpy(resultData.data(), base, output_clr.shape().size() * output_clr.dtype().size());
-                tst_limit = output_clr.shape().size();
-            }
         }
         break;
     }
-
-    // test on GPU
-    if(device == eDeviceType::GPU) {
-        auto* base = outputTensorData->basePtr();
-        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(resultData.data(), base, output_image_size, hipMemcpyDeviceToHost, stream));
-        HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
-        tst_limit = output_image_size;
+    // results vector
+    std::vector<BT> result;
+    result.resize(output_image_size);
+    // Copy data from output tensor into a host allocated results vector
+    switch(conversionCode) {
+        case eColorConversionCode::COLOR_RGB2GRAY:
+        case eColorConversionCode::COLOR_BGR2GRAY:
+            CopyTensorIntoVector(result, output_gry);
+        break;
+        default:
+            CopyTensorIntoVector(result, output_clr);
+        break;
     }
-    // final test
-    for (int i = 0; i < tst_limit; i++) {
-        float err = std::abs(resultData[i] - expectedData[i]);
-        if (err > 1) {
-            std::cout << "CvtColor " << ((device == eDeviceType::GPU) ? "(GPU)" : "(HOST)") << " failed at index: " << i << " with an error of: " << err << std::endl;
-            return eTestStatusType::UNEXPECTED_VALUE;
-        }
-    }
-    // clean up
-    if(device == eDeviceType::GPU) {
-        HIP_VALIDATE_NO_ERRORS(hipStreamDestroy(stream));
-    }
-
-    return eTestStatusType::TEST_SUCCESS;
+    // Compare data in actual output versus the generated golden reference image
+    CompareVectorsNear(result, ref);
 }
-
 }  // namespace
 
 eTestStatusType test_op_cvt_color(int argc, char **argv) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <test data path>" << std::endl;
-        return eTestStatusType::TEST_FAILURE;
-    }
-    fs::path testDataPath = fs::path(argv[1]) / "tests" / "ops";
-    fs::path tst_i = testDataPath / "test_input.bmp";
-    fs::path tst_y = testDataPath / "expected_color_cvt_yuv.bmp";
-    eTestStatusType success = eTestStatusType::TEST_SUCCESS;
-    eDeviceType gpu = eDeviceType::GPU;
-    eDeviceType cpu = eDeviceType::CPU;
+    TEST_CASES_BEGIN();
 
-    try {
-        cv::Mat expectedDataYUV = cv::imread(testDataPath / "expected_color_cvt_yuv.bmp");
-        cv::Mat expectedDataBGR = cv::imread(testDataPath / "expected_color_cvt_bgr.bmp");
-        cv::Mat expectedDataRGB = cv::imread(testDataPath / "expected_color_cvt_rgb.bmp");
-        cv::Mat expectedDataGrayscale = cv::imread(testDataPath / "expected_color_cvt_grayscale.bmp", cv::IMREAD_UNCHANGED);
+    // GPU correctness tests
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2YUV , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2YUV , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_YUV2RGB , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_YUV2BGR , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2BGR , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2RGB , eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2GRAY, eDeviceType::GPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2GRAY, eDeviceType::GPU));
 
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataYUV.data, eColorConversionCode::COLOR_BGR2YUV, gpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_y, expectedDataBGR.data, eColorConversionCode::COLOR_YUV2BGR, gpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataRGB.data, eColorConversionCode::COLOR_BGR2RGB, gpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataGrayscale.data, eColorConversionCode::COLOR_BGR2GRAY, gpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataYUV.data, eColorConversionCode::COLOR_BGR2YUV, cpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_y, expectedDataBGR.data, eColorConversionCode::COLOR_YUV2BGR, cpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataRGB.data, eColorConversionCode::COLOR_BGR2RGB, cpu), success);
-        EXPECT_TEST_STATUS(testCorrectness(tst_i, expectedDataGrayscale.data, eColorConversionCode::COLOR_BGR2GRAY, cpu), success);
-    } catch (Exception e) {
-        std::cout << "Exception: " << e.what() << std::endl;
-        return eTestStatusType::TEST_FAILURE;
-    }
-    return success;
+    // CPU correctness tests
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2YUV , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2YUV , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_YUV2RGB , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_YUV2BGR , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2BGR , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2RGB , eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_RGB2GRAY, eDeviceType::CPU));
+    TEST_CASE(TestCorrectness<uint8_t>(1, 180, 60, FMT_RGB8, COLOR_BGR2GRAY, eDeviceType::CPU));
+
+    TEST_CASES_END();
 }
