e51dd9aeaae923710da07d96fbf39534eb0df910
Zachary Vincze <zavincze@amd.com>
2025-08-22 16:15:27 -0400

    [Testing] NMS Conformance tests (#43)
    * Begin NMS golden implementation
    
    * Add test cases for NonMaximumSuppression
    
    * Switch from greedy NMS implementation for golden model/fix bugs in GPU/CPU implementations
    
    * Add test case with large batch size
    
    * Small comment update
    
    * Fix bug in IoU calculations
    
    ---------
    
    Co-authored-by: Jeff Jiang <142832361+jeffqjiangNew@users.noreply.github.com>

--- tests/roccv/cpp/test_op_non_max_suppression.cpp
+++ tests/roccv/cpp/test_op_non_max_suppression.cpp
@@ -20,11 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <algorithm>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <iterator>
 #include <op_non_max_suppression.hpp>
 #include <vector>
 
@@ -33,64 +28,188 @@ THE SOFTWARE.
 using namespace roccv;
 using namespace roccv::tests;
 
-eTestStatusType testCorrectness(std::vector<short4> boxes_data, std::vector<float> scores_data,
-                                std::vector<uint8_t> expected_data, float score_threshold, float iou_threshold,
-                                eDeviceType device) {
-    int numBoxes = scores_data.size();
-    TensorShape shape(TensorLayout(TENSOR_LAYOUT_NW), {1, numBoxes});
-    Tensor input(shape, DataType(DATA_TYPE_4S16), device);
-    Tensor output(shape, DataType(DATA_TYPE_U8), device);
-    Tensor scores(shape, DataType(DATA_TYPE_F32), device);
+namespace {
 
-    copyData<short4>(input, boxes_data, device);
-    copyData<float>(scores, scores_data, device);
+/**
+ * @brief Computes the area for a given box.
+ *
+ * @param[in] box The short4 representation of the box (x, y, width, height)
+ * @return The computed area of the box.
+ */
+float GoldenArea(const short4 &box) { return box.z * box.w; }
 
-    NonMaximumSuppression op;
-    op(nullptr, input, output, scores, score_threshold, iou_threshold, device);
-    hipDeviceSynchronize();
+/**
+ * @brief Computes the IoU between two boxes.
+ *
+ * @param[in] a Box A.
+ * @param[in] b Box B.
+ * @return The IoU between boxes A and B.
+ */
+float GoldenIoU(const short4 &a, const short4 &b) {
+    int aInterLeft = std::max(a.x, b.x);
+    int bInterTop = std::max(a.y, b.y);
+    int aInterRight = std::min(a.x + a.z, b.x + b.z);
+    int bInterBottom = std::min(a.y + a.w, b.y + b.w);
+    int widthInter = aInterRight - aInterLeft;
+    int heightInter = bInterBottom - bInterTop;
+    float interArea = widthInter * heightInter;
+    float iou = 0.0f;
 
-    return compareArray<uint8_t>(output, expected_data, 0.0f);
+    if (widthInter > 0.0f && heightInter > 0.0f) {
+        float unionArea = GoldenArea(a) + GoldenArea(b) - interArea;
+        if (unionArea > 0.0f) {
+            iou = interArea / unionArea;
+        }
+    }
+
+    return iou;
 }
 
+/**
+ * @brief Golden model for the non-maximum suppression operator. This calculates which boxes should be kept based on
+ * input boxes, scores for each box, and a global score/iou threshold. Non-maximum suppression for each batch index is
+ * calculated independently. This is a single-pass, naive parallel algorithm which tends to over suppress, not
+ * equivalent to the greedy NMS algorithm.
+ *
+ * @param[in] input A list of input boxes to check. Each short4 corresponds to the coordinates of a single box (x = x, y
+ * = y, z = width, w = height).
+ * @param[in] scores A list of scores corresponding to each box provided in input.
+ * @param[in] scoreThreshold The score threshold to use when determining which boxes to keep and discard.
+ * @param[in] iouThreshold The IoU threshold to use when comparing box intersections.
+ * @param[in] batchSize The size of the batch.
+ * @param[in] numBoxes The number of boxes per batch index.
+ * @return A list of U8 values corresponding to each input box. A value of 0 means the box was suppressed, a value of 1
+ * means the box is kept.
+ */
+std::vector<unsigned char> GoldenNonMaximumSuppression(std::vector<short4> input, std::vector<float> scores,
+                                                       float scoreThreshold, float iouThreshold, int batchSize,
+                                                       int numBoxes) {
+    std::vector<unsigned char> output(batchSize * numBoxes);
+
+    for (int batchIdx = 0; batchIdx < batchSize; batchIdx++) {
+        for (int a = 0; a < numBoxes; a++) {
+            // Starting index for this particular sample
+            size_t beginIdx = batchIdx * numBoxes;
+
+            const float &scoreA = scores.at(beginIdx + a);
+            unsigned char &dst = output.at(beginIdx + a);
+
+            // Discard this box immediately if its score is below the score threshold
+            if (scoreA < scoreThreshold) {
+                dst = 0u;
+                continue;
+            }
+
+            const short4 &boxA = input.at(beginIdx + a);
+            bool discard = false;
+
+            // Compare this box to all other boxes to determine if it should be suppressed or not
+            for (int b = 0; b < numBoxes; b++) {
+                if (a == b) {
+                    continue;
+                }
+
+                // For a box to be suppressed, the following conditions must be met
+                // 1. Its intersection over union with another box must be greater than iouThreshold. If the IoU is over
+                // the threshold, then check:
+                //    - If its confidence score is less than the box being compared against, suppress it.
+                //    - In the case of a tie breaker (scores are the same), suppress it if its area is less than the box
+                //      being compared with.
+                const short4 &boxB = input.at(beginIdx + b);
+                if (GoldenIoU(boxA, boxB) > iouThreshold) {
+                    const float &scoreB = scores.at(beginIdx + b);
+                    if (scoreA < scoreB || (scoreA == scoreB && GoldenArea(boxA) < GoldenArea(boxB))) {
+                        discard = true;
+                        break;
+                    }
+                }
+            }
+
+            dst = discard ? 0u : 1u;
+        }
+    }
+
+    return output;
+}
+
+/**
+ * @brief Generate input boxes for NMS in a way that's more likely for them to intersect with each other.
+ *
+ * @param batchSize The number of samples in the batch.
+ * @param numBoxes The number of boxes per sample.
+ * @param seed A seed for random generation. Defaults to 12345.
+ * @return A vector containing data for the generated boxes.
+ */
+std::vector<short4> GenerateBoxes(int batchSize, int numBoxes, int seed = 12345) {
+    std::vector<short4> output;
+    output.reserve(batchSize * numBoxes);
+
+    std::mt19937 eng(seed);
+    std::uniform_int_distribution<short> pos(0, 300);
+    std::uniform_int_distribution<short> size(50, 100);
+
+    for (int b = 0; b < batchSize; b++) {
+        for (int i = 0; i < numBoxes; i++) {
+            // Insert a randomly generated box (format is x, y, width, height)
+            output.push_back(make_short4(pos(eng), pos(eng), size(eng), size(eng)));
+        }
+    }
+
+    return output;
+}
+
+void TestCorrectness(int batchSize, int numBoxes, float scoreThreshold, float iouThreshold, eDeviceType device) {
+    // Create required input/output tensors for roccv::NonMaximumSuppression
+    Tensor inputTensor(TensorShape(TensorLayout(TENSOR_LAYOUT_NWC), {batchSize, numBoxes, 4}), DataType(DATA_TYPE_S16),
+                       device);
+    Tensor scoresTensor(TensorShape(TensorLayout(TENSOR_LAYOUT_NWC), {batchSize, numBoxes, 1}), DataType(DATA_TYPE_F32),
+                        device);
+    Tensor outputTensor(TensorShape(TensorLayout(TENSOR_LAYOUT_NWC), {batchSize, numBoxes, 1}), DataType(DATA_TYPE_U8),
+                        device);
+
+    // Generate data for input boxes and their associated scores
+    std::vector<short4> boxes = GenerateBoxes(batchSize, numBoxes);
+    std::vector<float> scores(batchSize * numBoxes);
+    FillVector(scores);
+
+    // Copy host-allocated vectors into tensors
+    CopyVectorIntoTensor(inputTensor, boxes);
+    CopyVectorIntoTensor(scoresTensor, scores);
+
+    hipStream_t stream;
+    HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
+    NonMaximumSuppression op;
+    op(stream, inputTensor, outputTensor, scoresTensor, scoreThreshold, iouThreshold, device);
+    HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
+    HIP_VALIDATE_NO_ERRORS(hipStreamDestroy(stream));
+
+    // Copy output tensor data into host allocated tensor
+    std::vector<unsigned char> actualOutput(outputTensor.shape().size());
+    CopyTensorIntoVector(actualOutput, outputTensor);
+
+    // Generate golden output using defined model above
+    std::vector<unsigned char> goldenOutput =
+        GoldenNonMaximumSuppression(boxes, scores, scoreThreshold, iouThreshold, batchSize, numBoxes);
+
+    CompareVectors(actualOutput, goldenOutput);
+}
+
+}  // namespace
+
 eTestStatusType test_op_non_max_suppression(int argc, char **argv) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <test data path>" << std::endl;
-        return eTestStatusType::TEST_FAILURE;
-    }
-    std::filesystem::path test_data = std::filesystem::path(argv[1]) / "tests" / "ops" / "expected_nms.bin";
-    std::ifstream file(test_data, std::ios::binary);
-    if (!file) {
-        std::cerr << "Error opening test file: " << test_data << std::endl;
-        return eTestStatusType::TEST_FAILURE;
-    }
+    TEST_CASES_BEGIN();
 
-    std::vector<uint8_t> test_data_vec(std::istreambuf_iterator<char>(file), {});
-    file.close();
+    // GPU conformance tests
+    TEST_CASE((TestCorrectness(1, 4, 0.0f, 0.5f, eDeviceType::GPU)));
+    TEST_CASE((TestCorrectness(3, 64, 0.25f, 0.5f, eDeviceType::GPU)));
+    TEST_CASE((TestCorrectness(20, 837, 0.5f, 0.45f, eDeviceType::GPU)));
+    TEST_CASE((TestCorrectness(1000, 34, 0.24f, 0.8f, eDeviceType::GPU)));
 
-    // clang-format off
-    std::vector<short4> boxesData = {
-        make_short4(100, 100, 200, 200),
-        make_short4(110, 110, 210, 210),
-        make_short4(220, 220, 320, 320),
-        make_short4(50, 50, 150, 150)
-    };
-    // clang-format on
+    // CPU conformance tests
+    TEST_CASE((TestCorrectness(1, 4, 0.0f, 0.5f, eDeviceType::CPU)));
+    TEST_CASE((TestCorrectness(3, 64, 0.25f, 0.5f, eDeviceType::CPU)));
+    TEST_CASE((TestCorrectness(20, 837, 0.5f, 0.45f, eDeviceType::CPU)));
+    TEST_CASE((TestCorrectness(1000, 34, 0.24f, 0.8f, eDeviceType::CPU)));
 
-    std::vector<float> scoresData = {0.9, 0.8, 0.85, 0.7};
-    float iouThreshold = 0.5f;
-    float scoreThreshold = 0.0f;
-
-    try {
-        EXPECT_TEST_STATUS(
-            testCorrectness(boxesData, scoresData, test_data_vec, scoreThreshold, iouThreshold, eDeviceType::GPU),
-            eTestStatusType::TEST_SUCCESS);
-        EXPECT_TEST_STATUS(
-            testCorrectness(boxesData, scoresData, test_data_vec, scoreThreshold, iouThreshold, eDeviceType::CPU),
-            eTestStatusType::TEST_SUCCESS);
-    } catch (Exception e) {
-        std::cerr << "Test failed with exception: " << e.what() << std::endl;
-        return eTestStatusType::TEST_FAILURE;
-    }
-
-    return eTestStatusType::TEST_SUCCESS;
+    TEST_CASES_END();
 }
