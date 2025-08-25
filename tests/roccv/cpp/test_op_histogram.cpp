0074b501887b93210c33ca3f374f022b0f6ca58a
svcbuild <paveltc>
2025-08-25 13:53:28 -0700

    removed unnecessary headers from tests

--- tests/roccv/cpp/test_op_histogram.cpp
+++ tests/roccv/cpp/test_op_histogram.cpp
@@ -26,10 +26,8 @@ THE SOFTWARE.
 #include "core/detail/type_traits.hpp"
 #include "core/detail/math/vectorized_type_math.hpp"
 #include <core/wrappers/image_wrapper.hpp>
-#include <filesystem>
 #include <iostream>
 #include <op_histogram.hpp>
-#include <opencv2/opencv.hpp>
 #include "operator_types.h"
 
 #include "test_helpers.hpp"
