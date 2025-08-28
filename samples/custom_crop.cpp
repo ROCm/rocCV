12b863c80b67517b0722504001915006933c2444
Jeff Jiang <jeffq.jiang@amd.com>
2025-08-28 10:39:52 -0400

    * rocCV/Custom crop sample: Added CPU path and logs.

--- samples/custom_crop.cpp
+++ samples/custom_crop.cpp
@@ -36,6 +36,7 @@ void ShowHelpAndExit(const char *option = NULL) {
     std::cout << "Options: " << option << std::endl
     << "-i Input File Path - required" << std::endl
     << "-o Output File Path - optional; default: output.bmp" << std::endl
+    << "-cpu Select CPU instead of GPU to perform operation - optional; default choice is GPU path" << std::endl
     << "-d GPU device ID (0 for the first device, 1 for the second, etc.) - optional; default: 0" << std::endl
     << "-crop Crop rectangle (top_left_corner_x, top_left_corner_y, width, height)- optional; default: use the set value in the app" << std::endl;
     exit(0);
@@ -44,16 +45,18 @@ void ShowHelpAndExit(const char *option = NULL) {
 int main(int argc, char** argv) {
     std::string input_file_path;
     std::string output_file_path = "output.bmp";
+    bool gpuPath = true; // use GPU by default
+    eDeviceType device = eDeviceType::GPU;
     int deviceId = 0;
     Box_t cropRect = {0, 0, 1, 1};
     bool cropSet = false;
 
     if(argc < 3) {
-        ShowHelpAndExit();
+        ShowHelpAndExit("-h");
     }
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-h")) {
-            ShowHelpAndExit();
+            ShowHelpAndExit("-h");
         }
         if (!strcmp(argv[i], "-i")) {
             if (++i == argc) {
@@ -81,52 +84,81 @@ int main(int argc, char** argv) {
             cropSet = true;
             continue;
         }
+        if (!strcmp(argv[i], "-cpu")) {
+            gpuPath = false;
+            continue;
+        }
     }
 
-    HIP_VALIDATE_NO_ERRORS(hipSetDevice(deviceId));
+    if (gpuPath) {
+        device = eDeviceType::GPU;
+        HIP_VALIDATE_NO_ERRORS(hipSetDevice(deviceId));
+    } else {
+        device = eDeviceType::CPU;
+    }
 
-    cv::Mat image_data = cv::imread(input_file_path);
-    if (image_data.empty()) {
+    cv::Mat imageData = cv::imread(input_file_path);
+    if (imageData.empty()) {
         std::cerr << "Failed to read the input image file" << std::endl;
         exit(1);
     }
     if (!cropSet) {
-        cropRect = {(image_data.cols / 4), (image_data.rows / 4), (image_data.cols / 2), (image_data.rows / 2)};
+        // Set a safe crop area if no user input
+        cropRect = {(imageData.cols / 4), (imageData.rows / 4), (imageData.cols / 2), (imageData.rows / 2)};
     }
 
     // Create input/output tensors for the image.
-    TensorShape inputShape(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, image_data.rows, image_data.cols, image_data.channels()});
+    TensorShape inputShape(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, imageData.rows, imageData.cols, imageData.channels()});
     DataType dtype(eDataType::DATA_TYPE_U8);
-    Tensor d_input(inputShape, dtype);
+    Tensor input(inputShape, dtype, device);
 
-    TensorShape outShape(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, cropRect.height, cropRect.width, image_data.channels()});
-    Tensor d_output(outShape, dtype);
+    TensorShape outShape(TensorLayout(eTensorLayout::TENSOR_LAYOUT_NHWC), {1, cropRect.height, cropRect.width, imageData.channels()});
+    Tensor output(outShape, dtype, device);
 
-    hipStream_t stream;
-    HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
+    hipStream_t stream = nullptr;
+    if (gpuPath) {
+        HIP_VALIDATE_NO_ERRORS(hipStreamCreate(&stream));
+    }
 
     // Move image data to input tensor
-    size_t image_size = d_input.shape().size() * d_input.dtype().size();
-    auto d_input_data = d_input.exportData<TensorDataStrided>();
-    HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(d_input_data.basePtr(), image_data.data, image_size, hipMemcpyHostToDevice, stream));
+    size_t imageSizeInByte = input.shape().size() * input.dtype().size();
+    auto input_data = input.exportData<TensorDataStrided>();
+    if (gpuPath) {
+        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(input_data.basePtr(), imageData.data, imageSizeInByte, hipMemcpyHostToDevice, stream));
+    } else {
+        memcpy(input_data.basePtr(), imageData.data, imageSizeInByte);
+    }
 
     CustomCrop op;
-    op(nullptr, d_input, d_output, cropRect, eDeviceType::GPU);
+    op(nullptr, input, output, cropRect, device);
 
     // Move image data back to host
-    size_t outputSize = d_output.shape().size() * d_output.dtype().size();
-    auto d_out_data = d_output.exportData<TensorDataStrided>();
+    size_t outputSize = output.shape().size() * output.dtype().size();
+    auto outData = output.exportData<TensorDataStrided>();
     std::vector<uint8_t> h_output(outputSize);
-    HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(h_output.data(), d_out_data.basePtr(), outputSize, hipMemcpyDeviceToHost, stream));
-
-    HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
+    if (gpuPath) {
+        HIP_VALIDATE_NO_ERRORS(hipMemcpyAsync(h_output.data(), outData.basePtr(), outputSize, hipMemcpyDeviceToHost, stream));
+        HIP_VALIDATE_NO_ERRORS(hipStreamSynchronize(stream));
+    } else {
+        memcpy(h_output.data(), outData.basePtr(), outputSize);
+    }
 
     // Write output image to disk
-    cv::Mat output_image_data(cropRect.height, cropRect.width, image_data.type(), h_output.data());
-    bool ret = cv::imwrite(output_file_path, output_image_data);
+    cv::Mat output_imageData(cropRect.height, cropRect.width, imageData.type(), h_output.data());
+    bool ret = cv::imwrite(output_file_path, output_imageData);
     if (!ret) {
         std::cerr << "Faild to save output image to the file" << std::endl;
     }
+
+    std::cout << "Input image file: " << input_file_path << std::endl;
+    std::cout << "Output image file: " << output_file_path << std::endl;
+    if (gpuPath) {
+        std::cout << "Operation on GPU device " << deviceId << std::endl;
+    } else {
+        std::cout << "Operation on CPU" << std::endl;
+    }
+    std::cout << "Input image size: width = " << imageData.cols << ", height = " << imageData.rows << std::endl;
+    std::cout << "Cropping area: top-left corner = (" << cropRect.x << ", " << cropRect.y << "), width = " << cropRect.width << ", height = " << cropRect.height << std::endl;
 
     return EXIT_SUCCESS;
 }
