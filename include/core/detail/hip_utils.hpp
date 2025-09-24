f9651833ed481f8e35a699fd1b94bed42e31cd3c
Zach Vincze <zavincze@amd.com>
2025-09-24 12:37:37 -0400

    Test prolonged lifetime of preprocessed rectangles

---
+++ include/core/detail/hip_utils.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+#include <hip/hip_runtime.h>
+
+#include <functional>
+#include <memory>
+
+namespace roccv::detail {
+namespace {
+static void StreamCallback(hipStream_t stream, hipError_t error, void* userData) {
+    std::function<void()>* func = static_cast<std::function<void()>*>(userData);
+    (*func)();
+    delete func;
+}
+};  // namespace
+
+/**
+ * @brief A non-blocking call which queues a host-side function call on the given stream. This call will execute only
+ * after all previous work in the queue has completed, and will block any future events in the stream until it has
+ * completed. Ensure no HIP calls are made in the callback function as doing so may cause deadlocks.
+ *
+ * @param[in] stream The stream to queue the function call on.
+ * @param[in] cb A lambda containing the function definition. E.g. `[&]() {...}`
+ */
+template <typename Callable>
+void AddHipStreamCallback(hipStream_t stream, Callable&& cb) {
+    std::function<void()>* data = new std::function<void()>(std::forward<Callable>(cb));
+    hipStreamAddCallback(stream, StreamCallback, data, 0);
+}
+}  // namespace roccv::detail
--- src/op_bnd_box.cpp
+++ src/op_bnd_box.cpp
@@ -27,11 +27,13 @@ THE SOFTWARE.
 #include <cstring>
 #include <functional>
 #include <iostream>
+#include <thread>
 #include <vector>
 
 #include "common/math_vector.hpp"
 #include "common/strided_data_wrap.hpp"
 #include "common/validation_helpers.hpp"
+#include "core/detail/hip_utils.hpp"
 #include "core/tensor.hpp"
 #include "core/wrappers/image_wrapper.hpp"
 #include "kernels/device/bnd_box_device.hpp"
@@ -44,7 +46,7 @@ BndBox::~BndBox() {}
 
 template <bool has_alpha, typename T>
 void dispatch_bnd_box_dtype(hipStream_t stream, const Tensor &input, const Tensor &output,
-                            const std::vector<Rect_t> &rects, const eDeviceType device) {
+                            std::shared_ptr<std::vector<Rect_t>> rects, const eDeviceType device) {
     ImageWrapper<T> inputWrapper(input);
     ImageWrapper<T> outputWrapper(output);
 
@@ -59,12 +61,12 @@ void dispatch_bnd_box_dtype(hipStream_t stream, const Tensor &input, const Tenso
             const auto zGridSize = batch_size;
 
             Rect_t *rects_ptr = nullptr;
-            const auto n_rects = rects.size();
+            const auto n_rects = rects->size();
 
             if (n_rects > 0) {
                 HIP_VALIDATE_NO_ERRORS(hipMallocAsync(&rects_ptr, sizeof(Rect_t) * n_rects, stream));
                 HIP_VALIDATE_NO_ERRORS(
-                    hipMemcpyAsync(rects_ptr, rects.data(), sizeof(Rect_t) * n_rects, hipMemcpyHostToDevice, stream));
+                    hipMemcpyAsync(rects_ptr, rects->data(), sizeof(Rect_t) * n_rects, hipMemcpyHostToDevice, stream));
             }
             Kernels::Device::bndbox_kernel<has_alpha, T>
                 <<<dim3(xGridSize, yGridSize, zGridSize), dim3(blockSize, blockSize, 1), 0, stream>>>(
@@ -72,11 +74,15 @@ void dispatch_bnd_box_dtype(hipStream_t stream, const Tensor &input, const Tenso
             if (n_rects > 0) {
                 HIP_VALIDATE_NO_ERRORS(hipFreeAsync(rects_ptr, stream));
             }
+
+            // Capture shared_ptr to rects in lambda to prolong lifetime up until operation has finished
+            detail::AddHipStreamCallback(
+                stream, [rects] { printf("BndBox finished, rects still alive. %lu\n", rects.use_count()); });
             break;
         }
 
         case eDeviceType::CPU: {
-            Kernels::Host::bndbox_kernel<has_alpha, T>(inputWrapper, outputWrapper, rects.data(), rects.size(),
+            Kernels::Host::bndbox_kernel<has_alpha, T>(inputWrapper, outputWrapper, rects->data(), rects->size(),
                                                        batch_size, height, width);
             break;
         }
@@ -108,13 +114,13 @@ void BndBox::operator()(hipStream_t stream, const Tensor &input, const Tensor &o
     const auto height = input.shape()[input.shape().layout().height_index()];
     const auto width = input.shape()[input.shape().layout().width_index()];
 
-    std::vector<Rect_t> rects;
-    generateRects(rects, bnd_boxes, height, width);
+    std::shared_ptr<std::vector<Rect_t>> rects = std::make_shared<std::vector<Rect_t>>();
+    generateRects(*rects, bnd_boxes, height, width);
 
     // Select kernel dispatcher based on number of channels and a base datatype.
     // clang-format off
     static const std::unordered_map<
-    eDataType, std::array<std::function<void(hipStream_t, const Tensor &, const Tensor &, const std::vector<Rect_t>&, const eDeviceType)>, 4>>
+    eDataType, std::array<std::function<void(hipStream_t, const Tensor &, const Tensor &, std::shared_ptr<std::vector<Rect_t>>, const eDeviceType)>, 4>>
         funcs =
         {
             {eDataType::DATA_TYPE_U8, {0, 0, dispatch_bnd_box_dtype<false, uchar3>, dispatch_bnd_box_dtype<true, uchar4>}},
