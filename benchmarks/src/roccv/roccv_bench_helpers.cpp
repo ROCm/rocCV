47e1933e0a47fadab49b389437aeb17f082f30f6
Zach Vincze <zavincze@amd.com>
2026-02-11 13:25:55 -0500

    Use rocRAND for benchmark data generation over needless host -> device copies

--- benchmarks/src/roccv/roccv_bench_helpers.cpp
+++ benchmarks/src/roccv/roccv_bench_helpers.cpp
@@ -22,69 +22,57 @@
 #include "roccv_bench_helpers.hpp"
 
 #include <core/hip_assert.h>
+#include <rocrand/rocrand.h>
 
 #include <roccvbench/utils.hpp>
-#include <vector>
 
-template <typename T>
-void MoveToTensor(const roccv::Tensor& tensor, const std::vector<T>& vec) {
-    auto tensor_data = tensor.exportData<roccv::TensorDataStrided>();
-    switch (tensor.device()) {
+namespace {
+static rocrand_generator GetGenerator(eDeviceType device) {
+    rocrand_generator gen;
+    switch (device) {
         case eDeviceType::GPU: {
-            HIP_VALIDATE_NO_ERRORS(hipMemcpy(tensor_data.basePtr(), vec.data(),
-                                             tensor.shape().size() * tensor.dtype().size(), hipMemcpyHostToDevice));
+            rocrand_create_generator(&gen, ROCRAND_RNG_PSEUDO_DEFAULT);
             break;
         }
-
         case eDeviceType::CPU: {
-            HIP_VALIDATE_NO_ERRORS(hipMemcpy(tensor_data.basePtr(), vec.data(),
-                                             tensor.shape().size() * tensor.dtype().size(), hipMemcpyHostToHost));
-            break;
-        }
-    }
-}
-
-void FillTensor(const roccv::Tensor& tensor) {
-    switch (tensor.dtype().etype()) {
-        case DATA_TYPE_U8: {
-            std::vector<uint8_t> vec = roccvbench::RandVector<uint8_t>(tensor.shape().size());
-            MoveToTensor<uint8_t>(tensor, vec);
-            break;
-        }
-
-        case DATA_TYPE_S8: {
-            std::vector<int8_t> vec = roccvbench::RandVector<int8_t>(tensor.shape().size());
-            MoveToTensor<int8_t>(tensor, vec);
-            break;
-        }
-
-        case DATA_TYPE_F32: {
-            std::vector<float> vec = roccvbench::RandVector<float>(tensor.shape().size());
-            MoveToTensor<float>(tensor, vec);
-            break;
-        }
-
-        case DATA_TYPE_F64: {
-            std::vector<double> vec = roccvbench::RandVector<double>(tensor.shape().size());
-            MoveToTensor<double>(tensor, vec);
-            break;
-        }
-
-        case DATA_TYPE_S32: {
-            std::vector<int32_t> vec = roccvbench::RandVector<int32_t>(tensor.shape().size());
-            MoveToTensor<int32_t>(tensor, vec);
-            break;
-        }
-
-        case DATA_TYPE_U32: {
-            std::vector<uint32_t> vec = roccvbench::RandVector<uint32_t>(tensor.shape().size());
-            MoveToTensor<uint32_t>(tensor, vec);
+            rocrand_create_generator_host_blocking(&gen, ROCRAND_RNG_PSEUDO_DEFAULT);
             break;
         }
 
         default: {
-            throw std::runtime_error("Unsupported tensor data type.");
-            break;
+            throw std::runtime_error("Unsupported device type.");
         }
     }
+
+    return gen;
+}
+
+template <typename T>
+void FillTensorImpl(const roccv::Tensor& tensor) {
+    const auto tensor_data = tensor.exportData<roccv::TensorDataStrided>();
+    const auto generator = GetGenerator(tensor.device());
+
+    if constexpr (std::is_integral_v<T>) {
+        rocrand_generate_char(generator, static_cast<unsigned char*>(tensor_data.basePtr()),
+                              tensor.shape().size() * tensor.dtype().size());
+    } else if constexpr (std::is_same_v<T, float>) {
+        rocrand_generate_uniform(generator, static_cast<float*>(tensor_data.basePtr()), tensor.shape().size());
+    } else if constexpr (std::is_same_v<T, double>) {
+        rocrand_generate_uniform_double(generator, static_cast<double*>(tensor_data.basePtr()), tensor.shape().size());
+    } else {
+        throw std::runtime_error("Unsupported data type.");
+    }
+    HIP_VALIDATE_NO_ERRORS(hipDeviceSynchronize());
+}
+}  // namespace
+
+void FillTensor(const roccv::Tensor& tensor) {
+    static const std::unordered_map<eDataType, void (*)(const roccv::Tensor&)> fillTensorImpls = {
+        {eDataType::DATA_TYPE_U8, FillTensorImpl<uint8_t>},   {eDataType::DATA_TYPE_S8, FillTensorImpl<int8_t>},
+        {eDataType::DATA_TYPE_U16, FillTensorImpl<uint16_t>}, {eDataType::DATA_TYPE_S16, FillTensorImpl<int16_t>},
+        {eDataType::DATA_TYPE_U32, FillTensorImpl<uint32_t>}, {eDataType::DATA_TYPE_S32, FillTensorImpl<int32_t>},
+        {eDataType::DATA_TYPE_F32, FillTensorImpl<float>},    {eDataType::DATA_TYPE_F64, FillTensorImpl<double>},
+    };
+
+    fillTensorImpls.at(tensor.dtype().etype())(tensor);
 }
