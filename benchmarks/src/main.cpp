d4c21b52f8f5a2b0cdfef771448c224fb01d6c5c
Zach Vincze <zavincze@amd.com>
2026-03-06 13:16:48 -0500

    Initialize strValue field for injected bench parameters

--- benchmarks/src/main.cpp
+++ benchmarks/src/main.cpp
@@ -338,11 +338,14 @@ int main(int argc, char** argv) {
                     roccvbench::BenchmarkParamsList params = benchmark.params;
 
                     // Inject config parameters into params
-                    params.push_back(roccvbench::BenchmarkParam{"samples", config.samples});
-                    params.push_back(roccvbench::BenchmarkParam{"height", config.height});
-                    params.push_back(roccvbench::BenchmarkParam{"width", config.width});
-                    params.push_back(roccvbench::BenchmarkParam{"runs", config.runs});
-                    params.push_back(roccvbench::BenchmarkParam{"warmupRuns", config.warmupRuns});
+                    params.push_back(
+                        roccvbench::BenchmarkParam{"samples", config.samples, std::to_string(config.samples)});
+                    params.push_back(
+                        roccvbench::BenchmarkParam{"height", config.height, std::to_string(config.height)});
+                    params.push_back(roccvbench::BenchmarkParam{"width", config.width, std::to_string(config.width)});
+                    params.push_back(roccvbench::BenchmarkParam{"runs", config.runs, std::to_string(config.runs)});
+                    params.push_back(
+                        roccvbench::BenchmarkParam{"warmupRuns", config.warmupRuns, std::to_string(config.warmupRuns)});
 
                     auto result = benchmark.func(params);
 
