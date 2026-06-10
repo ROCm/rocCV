a64b57d909c55b29faa8f44f8ad306f649bc32cd
daniellegillai <dagillai@amd.com>
2026-06-09 21:07:40 -0700

    skeleton for op_gaussian .cpp and .hpp

---
+++ include/op_gaussian.hpp
@@ -0,0 +1,84 @@
+/**
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <hip/hip_runtime.h>
+
+#include "core/tensor.hpp"
+#include "i_operator.hpp"
+#include "operator_types.h"
+
+namespace roccv {
+
+/**
+ * @brief Class for managing the Gaussian operator.
+ *
+ */
+class Gaussian final : public IOperator {
+   public:
+   // Stateful constructor to take in max width, max height, max batch size?
+    /**
+     * @brief Executes the Gaussian operation on the given HIP stream.
+     *
+     * TODO add in formula?
+     *
+     * Limitations:
+     *
+     * Input:
+     *       Supported TensorLayout(s): [NHWC, HWC]
+     *                        Channels: [1, 3, 4]
+     *       Supported DataType(s):     [U8, U16, S16, S32, F32]
+     *
+     * Output:
+     *       Supported TensorLayout(s): [NHWC, HWC]
+     *                        Channels: [1, 3, 4]
+     *       Supported DataType(s):     [U8, U16, S16, S32, F32]
+     *
+     * // TODO: kernel must be odd, or not provided (in which case it is calculated from sigma)
+     *
+     * Input/Output dependency:
+     *
+     *       Property      |  Input == Output
+     *      -------------- | -------------
+     *       TensorLayout  | Yes
+     *       DataType      | Yes
+     *       Channels      | Yes
+     *       Width         | Yes
+     *       Height        | Yes
+     *       Batch         | Yes
+     *
+     * @param[in] stream The HIP stream to run this operation on.
+     * @param[in] input Input tensor.
+     * @param[out] output Output tensor.
+     * @param[in] kernelWidth Gaussian kernel width.
+     * @param[in] kernelHeight Gaussian kernel height.
+     * @param[in] sigmaX Gaussian kernel standard deviation in X direction.
+     * @param[in] sigmaY Gaussian kernel standard deviation in Y direction.
+     * @param[in] borderMode A border type to identify the pixel extrapolation
+     * method (e.g. BORDER_TYPE_CONSTANT or BORDER_TYPE_REPLICATE).
+     * @param[in] device The device which this operation should run on. (Default: eDeviceType::GPU)
+     */
+    void operator()(hipStream_t stream, const Tensor& input, Tensor& output, int kernelWidth, int kernelHeight,
+                    double sigmaX, double sigmaY, eBorderType borderMode, eDeviceType device = eDeviceType::GPU) const;
+};
+}  // namespace roccv
---
+++ src/op_gaussian.cpp
@@ -0,0 +1,77 @@
+/**
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include <hip/hip_runtime.h>
+
+#include <functional>
+
+#include "common/validation_helpers.hpp"
+#include "core/detail/casting.hpp"
+#include "core/wrappers/image_wrapper.hpp"
+#include "op_thresholding.hpp"
+// #include "kernels/device/gaussian_device.hpp"
+// #include "kernels/host/gaussian_host.hpp"
+
+namespace roccv {
+
+// TODO: figure out if should group pair params
+
+// skeleton for final dispatch layer for kernel launch
+template <typename T>
+void dispatch_gaussian(hipStream_t stream, const Tensor& input, const Tensor& output, eDeviceType device) {
+    ImageWrapper<T> inputWrapper(input);
+    ImageWrapper<T> outputWrapper(output);
+
+    switch (device) {
+        case eDeviceType::GPU: {
+            dim3 block(64, 16);
+            dim3 grid((outputWrapper.width() + block.x - 1) / block.x, (outputWrapper.height() + block.y - 1) / block.y,
+                      outputWrapper.batches());
+            Kernels::Device::gaussian<<<grid, block, 0, stream>>>(inputWrapper, outputWrapper);
+            break;
+        }
+
+        case eDeviceType::CPU: {
+            Kernels::Host::gaussian(inputWrapper, outputWrapper);
+            break;
+        }
+    }
+}
+
+// skeleton
+void Gaussian::operator()(hipStream_t stream, const Tensor& input, const Tensor& output, int kernelWidth,
+                          int kernelHeight, double sigmaX, double sigmaY, eBorderType borderMode,
+                          eDeviceType device) const {
+    // TODO Validate
+
+    // clang-format off
+    static const std::unordered_map<
+    eDataType, std::array<std::function<void(hipStream_t stream, const Tensor& input, const Tensor& output,
+        eDeviceType device)>, 4>>
+        funcs = {
+//{{DISPATCH_TABLE}}
+        };
+    // clang-format on
+    auto func = funcs.at(input.dtype().etype())[input.shape(input.layout().channels_index()) - 1];
+    if (func == 0) throw Exception("Not mapped to a defined function.", eStatusType::INVALID_OPERATION);
+    func(stream, input, output, device);
+}
+}  // namespace roccv
