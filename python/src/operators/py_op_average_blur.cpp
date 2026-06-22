3c2f2bc5fec543aadc93fd6346b76d78f709a40f
daniellegillai <dagillai@amd.com>
2026-06-22 16:38:45 -0700

    python tests added and passing

--- python/src/operators/py_op_average_blur.cpp
+++ python/src/operators/py_op_average_blur.cpp
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#include "operators/py_op_average_blur.hpp"
+
 #include <op_average_blur.hpp>
 
 #include "py_helpers.hpp"
