15c190978f767c9a595274c12067467004a633f3
daniellegillai <dagillai@amd.com>
2026-06-30 14:08:31 -0700

    separable version only

--- include/op_average_blur.hpp
+++ include/op_average_blur.hpp
@@ -101,14 +101,10 @@ class AverageBlur : public IOperator {
     int32_t m_maxKernelHeight;
     DefaultAllocator m_allocator;
     std::mutex m_bufferMutex;
-    //float* m_hostKernelMem = nullptr;
-    //float* m_deviceKernelMem = nullptr;
-    ///*
     float* m_hostKernelMemH = nullptr;
     float* m_hostKernelMemV = nullptr;
     float* m_deviceKernelMemH = nullptr;
     float* m_deviceKernelMemV = nullptr;
-    //*/
     hipEvent_t m_completionEvent = nullptr;
 };
 }  // namespace roccv
