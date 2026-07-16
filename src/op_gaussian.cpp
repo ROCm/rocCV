c6d0f8d1e634db1f02a1bcf7fb621959b4afbc9e
daniellegillai <dagillai@amd.com>
2026-07-16 09:40:00 -0700

    moved filter_2d.hpp into details dir

--- src/op_gaussian.cpp
+++ src/op_gaussian.cpp
@@ -29,7 +29,7 @@ THE SOFTWARE.
 
 #include "common/validation_helpers.hpp"
 #include "core/detail/casting.hpp"
-#include "filter_2d.hpp"
+#include "details/filter_2d.hpp"
 
 namespace roccv {
 Gaussian::Gaussian(int32_t maxKernelWidth, int32_t maxKernelHeight)
